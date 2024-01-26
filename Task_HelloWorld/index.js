0c9cb3f1cc684609c89f25add5c084ee8584fb83
Parth <omparthchougale2182002@gmail.com>
2024-01-26 13:29:08 +0530

    task: Task: Creating a Simple JavaScript Program

--- Task_HelloWorld/index.js
+++ Task_HelloWorld/index.js
@@ -1,26 +1,46 @@
 // Print hello World
-// Write your code here
+console.log("Hello World");
+
 
 // Write your code above
 
 //Declare a variable i and set its value to 5
 // Write your code here
-
+let i=5;
+console.log(i);
 // Write your code above
 
 //Write the if else statement to console "Number is odd" if the value of i is odd and "Number is even" if the value of i is even.
 // Write your code here
-
+    let i=5;
+    if(i % 2===1){
+        console.log("Number is odd");
+    }
+    else{
+        console.log("Number is even");
+    }
 // Write your code above
 
 //Declare a string variable which is always constant and set is value to 'triangle'.
 // Write your code here
-
+const TRIANGLE = 'It has 3 sides'
+console.log(TRIANGLE);
 // Write your code above
 
 //Write a function 'findSide' which takes an argument 'type' and return the number of sides in that shape
 // Write your code here
+function findSide(type){
+    switch (type.toLowerCase()){
+        case 'triangle':
+        return 3;
+        case 'square':
+        return 4;
+        case 'circle':
+        return 'infinite;'
+    }
 
+
+}
 // Write your code above
 const side = findSide('Circle');
 console.log('Side', side);
