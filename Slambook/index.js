75e1ff3a990ef03e7b2a942ab61518d22ef53a60
Parth <omparthchougale2182002@gmail.com>
2024-01-31 10:00:17 +0530

    task: Project Task-Selecting form element

--- Slambook/index.js
+++ Slambook/index.js
@@ -1,17 +1,24 @@
 let form;
 let allSlamArray=[];
 //Write code below to select the form element
-
+form = document.querySelector ('.form');
 // Write your code above
 console.log(form,"form");
 
 // Add the evenlistner below
-
+if(form){
+    form.addEventListener('submit', function (event){
+        event.preventDefault();
+        showSlamList();
+    });
+}else{
+    console.log('Form element not found.');
+}
 //Write your code above
 
 function showSlamList(){
 //Update the function here
-
+console.log('Form Submitted!');
 // Write your code above this line
 }
 
